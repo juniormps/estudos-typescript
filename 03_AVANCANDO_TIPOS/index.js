4e8c0cfa93e0043cec4f20191a9b09e308eac6e5
Marcio Junior <mps.junior@hotmail.com>
2026-07-09 14:33:00 -0300

    Ajustes no código

--- 03_AVANCANDO_TIPOS/index.js
+++ 03_AVANCANDO_TIPOS/index.js
@@ -5,7 +5,7 @@ nums.push(5);
 console.log(nums[2]);
 // nums.push('a')
 // nums = 'teste'
-const nomes = ['Matheus', 'Pedro'];
+const nomes = ["Matheus", "Pedro"];
 // nomes.push(2)
 // 2 - outra sintaxe de array
 const numbers = [100, 200];
@@ -13,7 +13,7 @@ numbers.push(200);
 // numbers.push('teste')
 console.log(numbers[1]);
 // 3 - any
-const arr = [1, 'teste', true, { nome: 'Matheus' }];
+const arr = [1, "teste", true, { nome: "Matheus" }];
 console.log(arr);
 arr.push([1, 2, 3]);
 console.log(arr);
@@ -28,25 +28,28 @@ function greeting(name) {
     return `Olá ${name}!`;
     // return 1
 }
-console.log(greeting('Matheus'));
+console.log(greeting("Matheus"));
 // greeting(1)
 // 6 - funcoes anonimas
 setTimeout(function () {
     const sallary = 1000;
     // console.log(parseFloat(sallary))
-}, 100);
+    //console.log(sallary)
+}, 2000);
 // 7 - tipos de objetos
 function passCoordinates(coord) {
-    console.log('X coordinates: ' + coord.x);
-    console.log('X coordinates: ' + coord.y);
+    console.log("X coordinates: " + coord.x);
+    console.log("X coordinates: " + coord.y);
 }
 const objCoord = { x: 10, y: 5 };
 passCoordinates(objCoord);
 // 8 - propriedades opcionais
 function showNumbers(a, b, c) {
-    console.log('A: ' + a);
-    console.log('B: ' + b);
-    console.log('C: ' + c);
+    console.log("A: " + a);
+    console.log("B: " + b);
+    if (c !== undefined) {
+        console.log("C: " + c);
+    }
 }
 showNumbers(1, 2, 3);
 showNumbers(4, 8);
@@ -54,38 +57,38 @@ showNumbers(4, 8);
 // 9 - validando parâmetro opcional
 function advancedGreeting(firstName, lastName) {
     if (lastName !== undefined) {
-        console.log(`Olá, ${firstName} ${lastName}, tudo bem?`);
+        return console.log(`Olá, ${firstName} ${lastName}, tudo bem?`);
     }
-    console.log(`Olá, ${firstName}, tudo bem?`);
+    return console.log(`Olá, ${firstName}, tudo bem?`);
 }
-advancedGreeting('Matheus', 'Battisti');
-advancedGreeting('João');
+advancedGreeting("Matheus", "Battisti");
+advancedGreeting("João");
 // 10 - union type
 function showBalance(balance) {
     console.log(`O saldo da conta é R$${balance}`);
 }
 showBalance(500);
-showBalance('100');
+showBalance("100");
 // 11 - mais sobre union types
 function showUserRole(role) {
-    if (typeof role === 'boolean') {
-        console.log('Usuário não aprovado!');
+    if (typeof role === "boolean") {
+        return console.log("Usuário não aprovado!");
     }
-    console.log(`O usuário é um: ${role}`);
+    return console.log(`O usuário é um: ${role}`);
 }
 showUserRole(false);
-showUserRole('Admin');
-showUserRole('Editor');
+showUserRole("Admin");
+showUserRole("Editor");
 function showId(id) {
     console.log(`O ID é: ${id}`);
 }
-showId('15');
+showId("15");
 showId(20);
 function userDetails(user) {
     console.log(`Nome: ${user.name}`);
     console.log(`Sobrenome: ${user.surname}`);
 }
-userDetails({ name: 'Matheus', surname: 'Battisti' });
+userDetails({ name: "Matheus", surname: "Battisti" });
 function showCoords(obj) {
     console.log(`x: ${obj.x}, y: ${obj.y}, z: ${obj.z}`);
 }
@@ -95,24 +98,23 @@ const itemCoords = {
     z: 12,
 };
 showCoords(itemCoords);
-const randomNumber = 10;
-const somePerson = { name: 'João', age: 25 };
+const somePerson = { name: "João", age: 25 };
 console.log(somePerson);
 // type personType = {
 //     age: number
 // }
 // 15 - literal types
 let test;
-test = 'testando';
+test = "testando";
 // test = "opa"
 function showDirection(direction) {
     console.log(`The direction is: ${direction}`);
 }
-showDirection('left');
-showDirection('center');
+showDirection("left");
+showDirection("center");
 //showDirection("up")
 // 16 - non-null assertion
-const p = document.getElementById('some-p');
+const p = document.getElementById("some-p");
 console.log(p.innerText);
 // 17 - big int
 let n;
@@ -121,6 +123,7 @@ n = 1000n;
 // console.log(n + 1)
 console.log(n + 10n);
 // 17 - symbol
-let symbolA = Symbol('a');
-let symbolB = Symbol('a');
+let symbolA = Symbol("a");
+let symbolB = Symbol("a");
 console.log(symbolA === symbolB);
+console.log(symbolA == symbolB);
