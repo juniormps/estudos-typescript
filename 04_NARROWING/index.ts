39230bc1863dd8830b86997c869f0258c8407443
Marcio Junior <mps.junior@hotmail.com>
2026-07-09 15:37:27 -0300

    Adição de comentários e pequenos ajustes no código

--- 04_NARROWING/index.ts
+++ 04_NARROWING/index.ts
@@ -1,105 +1,114 @@
+// Narrowing são formas de checar o tipo de uma variável, para que possamos trabalhar com ela de forma mais segura.
+
 // 1 - type guard
 function sum(a: number | string, b: number | string) {
-  if (typeof a === 'string' && typeof b === 'string') {
-    console.log(parseFloat(a) + parseFloat(b))
-  } else if (typeof a === 'number' && typeof b === 'number') {
-    console.log(a + b)
-  } else {
-    console.log('Impossível realizar a soma!')
-  }
+    if (typeof a === "string" && typeof b === "string") {
+        console.log(parseFloat(a) + parseFloat(b));
+    } else if (typeof a === "number" && typeof b === "number") {
+        console.log(a + b);
+    } else {
+        console.log("Impossível realizar a soma!");
+    }
 }
 
-sum(1, 2)
-sum('4', '4')
-sum('5', 1)
+sum(1, 2);
+sum("4", "4");
+sum("5", 1);
 
 // 2 - checando se valor existe
 
-function operations(arr: number[], operation: string | undefined) {
-  if (operation) {
-    if (operation === 'sum') {
-      const sum = arr.reduce((i, total) => i + total)
-      console.log(sum)
-    } else if (operation === 'multiply') {
-      const multiply = arr.reduce((i, total) => i * total)
-      console.log(multiply)
+function operations(arr: number[], operation?: string | undefined) {
+    if (operation) {
+        if (operation === "sum") {
+            const sum = arr.reduce((i, total) => i + total);
+            console.log(sum);
+
+        } else if (operation === "multiply") {
+            const multiply = arr.reduce((i, total) => i * total);
+            console.log(multiply);
+        }
+
+    } else {
+        console.log("Por favor, defina uma operação!");
     }
-  } else {
-    console.log('Por favor, defina uma operação!')
-  }
 }
 
-operations([10, 20, 30], 'sum')
-operations([10, 20, 30], 'multiply')
-operations([10, 20, 30], '')
+operations([10, 20, 30], "sum");
+operations([10, 20, 30], "multiply");
+operations([10, 20, 30], "");
 
 // 3 - instanceof
+// instanceof é um operador que verifica se um objeto é uma instância de uma classe específica.
 class User {
-  name
+    name;
 
-  constructor(name: string) {
-    this.name = name
-  }
+    constructor(name: string) {
+        this.name = name;
+    }
 }
 
 class SuperUser extends User {
-  constructor(name: string) {
-    super(name)
-  }
+    constructor(name: string) {
+        super(name);
+    }
 }
 
-const jhon = new User('Jhon')
-const paul = new SuperUser('Paul')
+const jhon = new User("Jhon");
+const paul = new SuperUser("Paul");
+
+console.log(jhon);
+console.log(paul);
 
 function userGreeting(user: object) {
-  if (user instanceof SuperUser) {
-    console.log(`Olá ${user.name}, deseja ver os dados?`)
-  } else if (user instanceof User) {
-    console.log(`Olá ${user.name}!`)
-  }
+    if (user instanceof SuperUser) {
+        console.log(`Olá ${user.name}, deseja ver os dados?`);
+    } else if (user instanceof User) {
+        console.log(`Olá ${user.name}!`);
+    }
 }
 
-userGreeting(jhon)
-userGreeting(paul)
+userGreeting(jhon);
+userGreeting(paul);
 
 // 4 - operador in
+// O operador in é usado para verificar se uma propriedade específica existe em um objeto.
 class Dog {
-  name
-  breed
+    name;
+    breed;
 
-  constructor(name: string, breed?: string) {
-    this.name = name
-    if (breed) {
-      this.breed = breed
+    constructor(name: string, breed?: string) {
+        this.name = name;
+        if (breed) {
+            this.breed = breed;
+        }
     }
-  }
 }
 
-const turca = new Dog('Turca')
-const bob = new Dog('Bob', 'Pastor Alemão')
+const turca = new Dog("Turca");
+const bob = new Dog("Bob", "Pastor Alemão");
 
 function showDogDetails(dog: Dog) {
-  if ('breed' in dog) {
-    console.log(`O cachorro é da raça: ${dog.breed}`)
-  } else {
-    console.log('O cachorro é um SRD!')
-  }
+    if ("breed" in dog) {
+        console.log(`O cachorro é da raça: ${dog.breed}`);
+    } else {
+        console.log("O cachorro é um SRD!");
+    }
 }
 
-showDogDetails(turca)
-showDogDetails(bob)
+showDogDetails(turca);
+showDogDetails(bob);
 
 // 5 - desafio
-type Review = number | boolean
+type Review = number | boolean;
 
 function showUserReview(review: Review) {
-  if (!review) {
-    console.log('Você não avaliou o produto!')
-    return
-  }
-  console.log(`A nota que você deu foi ${review}, obrigado!`)
+    if (!review) {
+        console.log("Você não avaliou o produto!");
+        return;
+    }
+    console.log(`A nota que você deu foi ${review}, obrigado!`);
 }
 
-showUserReview(false)
-showUserReview(3)
-showUserReview(4)
+showUserReview(false);
+showUserReview(3);
+showUserReview(4);
